9ba9bcfdeb73c2e00b8d6b58e5e7f402ed03c307
Florent Legeron <legeronflo@hotmail.fr>
2019-03-15 17:00:37 +0100

    delete useless field

--- db/migrate/20190315152327_add_condtions_validation_to_users.rb
+++
@@ -1,5 +0,0 @@
-class AddCondtionsValidationToUsers < ActiveRecord::Migration[5.2]
-  def change
-    add_column :users, :condtions_validation, :boolean, default: false
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_15_152327) do
+ActiveRecord::Schema.define(version: 2019_03_15_155851) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -42,7 +42,7 @@ ActiveRecord::Schema.define(version: 2019_03_15_152327) do
     t.datetime "updated_at", null: false
     t.string "first_name"
     t.string "last_name"
-    t.boolean "condtions_validation", default: false
+    t.boolean "conditions_validation", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
