78ac12172046ef147ad91c6d0de639986b161113
Florent Legeron <legeronflo@hotmail.fr>
2019-03-18 15:58:56 +0100

    did migration

---
+++ db/migrate/20190318144929_create_user_measures.rb
@@ -0,0 +1,11 @@
+class CreateUserMeasures < ActiveRecord::Migration[5.2]
+  def change
+    create_table :user_measures do |t|
+      t.references :measure, foreign_key: true
+      t.references :user, foreign_key: true
+      t.integer :value
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,28 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_27_151135) do
+ActiveRecord::Schema.define(version: 2019_03_18_144929) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "measures", force: :cascade do |t|
+    t.string "name"
+    t.string "video"
+    t.string "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "user_measures", force: :cascade do |t|
+    t.bigint "measure_id"
+    t.bigint "user_id"
+    t.integer "value"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["measure_id"], name: "index_user_measures_on_measure_id"
+    t.index ["user_id"], name: "index_user_measures_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -23,8 +41,13 @@ ActiveRecord::Schema.define(version: 2019_02_27_151135) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "first_name"
+    t.string "last_name"
+    t.boolean "conditions_validation", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "user_measures", "measures"
+  add_foreign_key "user_measures", "users"
 end
