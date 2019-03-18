4576f6eb4e712f3ef549a9669be792f224765635
Florent Legeron <legeronflo@hotmail.fr>
2019-03-18 10:26:58 +0100

    add routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   get '/legal', to: 'pages#legal', as: 'legal'
   root to: 'pages#home'
+  resources :mesures, only: [:index, :show]
+  resources :mesures, only: [:new, :create]
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
