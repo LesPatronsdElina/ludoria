929675e43193a0f5fc694a4ae92d47e315d614c3
Florent Legeron <legeronflo@hotmail.fr>
2019-04-01 15:48:32 +0200

    add a mailer template

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,5 @@
 class ApplicationController < ActionController::Base
   # include Pundit
-  before_action :initialize_contact
   protect_from_forgery with: :exception
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
@@ -19,9 +18,6 @@ class ApplicationController < ActionController::Base
 
     devise_parameter_sanitizer.permit(:sign_up, keys: user_data)
     devise_parameter_sanitizer.permit(:account_update, keys: user_data)
-  end
-  def initialize_contact
-    @contact = Contact.new
   end
   # private
   # def skip_pundit?
