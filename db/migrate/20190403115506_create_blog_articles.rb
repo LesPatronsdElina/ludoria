80938634ab5aeb88d0b29735c0e5824d16bd7755
Florent Legeron <legeronflo@hotmail.fr>
2019-04-03 16:55:11 +0200

    change string to text

--- db/migrate/20190403115506_create_blog_articles.rb
+++ db/migrate/20190403115506_create_blog_articles.rb
@@ -2,7 +2,7 @@ class CreateBlogArticles < ActiveRecord::Migration[5.2]
   def change
     create_table :blog_articles do |t|
       t.string :title
-      t.string :content
+      t.text :content
       t.string :cover_picture
 
       t.timestamps
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_03_125323) do
+ActiveRecord::Schema.define(version: 2019_04_03_145155) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,7 +26,7 @@ ActiveRecord::Schema.define(version: 2019_04_03_125323) do
 
   create_table "blog_articles", force: :cascade do |t|
     t.string "title"
-    t.string "content"
+    t.text "content"
     t.string "cover_picture"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
