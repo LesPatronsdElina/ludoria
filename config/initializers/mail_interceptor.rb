5ab1d0851b777e118a60ff8fe75ea92cae872173
Florent Legeron <legeronflo@hotmail.fr>
2019-04-03 09:37:09 +0200

    front-log-in

--- config/initializers/mail_interceptor.rb
+++ config/initializers/mail_interceptor.rb
@@ -1,5 +1,5 @@
 options = { forward_emails_to: 'intercepted_emails@domain.com' }
-if Rails.env.production?
+if Rails.env.production? || Rails.env.staging?
   interceptor = MailInterceptor::Interceptor.new(options)
   ActionMailer::Base.register_interceptor(interceptor)
 end
