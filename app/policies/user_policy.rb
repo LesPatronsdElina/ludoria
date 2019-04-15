4311e8c6d093742a867ed858db2ddbf262f702a4
Florent Legeron <legeronflo@hotmail.fr>
2019-04-15 10:53:58 +0200

    delete user policy

--- app/policies/user_policy.rb
+++
@@ -1,8 +0,0 @@
-class UserPolicy < ApplicationPolicy
-  # overriding user from ApplicationPolicy because i need the acting_as user id,
-  # not the specific one.
-
-  def update?
-    return true
-  end
-end
