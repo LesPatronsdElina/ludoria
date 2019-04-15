f465760d27b1bdece3811f5e49f4bc1c313e12c3
Florent Legeron <legeronflo@hotmail.fr>
2019-04-15 15:06:45 +0200

    add requests test

--- spec/requests/blog_articles_spec.rb
+++ spec/requests/blog_articles_spec.rb
@@ -3,13 +3,13 @@ require 'rails_helper'
 RSpec.describe "BlogArticlesController Integration", type: :request do
   describe "GET /blog#index" do
     it "When go to blog it display a page" do
-      get "/blog"
+      get "/blog_articles"
       expect(response).to have_http_status(200)
     end
   end
     describe "GET /blog#show" do
     it "When go to blog it display a page" do
-      get "/blog/1"
+      get "/blog_articles/#{BlogArticle.first}"
       expect(response).to have_http_status(200)
     end
   end
