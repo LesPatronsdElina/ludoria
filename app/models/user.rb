58e7cf34505472c27e24aaf2436b7aa3d978d97e
Florent Legeron <legeronflo@hotmail.fr>
2019-04-25 19:01:47 +0200

    comment confirmable

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   after_create :create_user_measures
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
-         :confirmable
+         # :confirmable
   has_many :user_measures, dependent: :destroy
   accepts_nested_attributes_for :user_measures
 
