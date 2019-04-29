8d53776c7d0598622cc464781f7e03e70184d8ad
Claudine Pradal <claudinecartailler@gmail.com>
2019-04-29 15:31:56 +0200

    add capital letter

--- app/admin/blog_articles.rb
+++ app/admin/blog_articles.rb
@@ -57,7 +57,7 @@ ActiveAdmin.register BlogArticle do
         f.button :submit
         end
       end
-      tab 'photos de l Article' do
+      tab 'Photos de l Article' do
         f.inputs do
           f.has_many :blog_photos, heading: 'Photo Details', allow_destroy: true do |photo|
             photo.input :photo, as: :file, hint: cl_image_tag(photo.object.photo.url),  label: "Importer une photo"
